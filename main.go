77e70f9995a5e088f99c3735fc526215961b9e81
MarySmirnova <marysmirnova_work@mail.ru>
2021-12-17 04:18:25 +1000

    working algorithm

--- main.go
+++ main.go
@@ -6,10 +6,10 @@ import (
 )
 
 func main() {
-	tow := NewTower([]int{1, 5, 3, 6, 2, 4, 8, 7})
+	tow, n := NewTower([]int{1, 5, 3, 6, 2, 4, 8, 7, 9, 10, 12, 11, 13, 1, 5})
 	fmt.Println(tow)
-	Hanoi(0, &tow, startArr)
 
+	tow.Hanoi(&tow.start, &tow.end, &tow.tmp, n)
 	fmt.Println(tow)
 }
 
@@ -19,65 +19,25 @@ type Tower struct {
 	tmp   []int
 }
 
-func NewTower(arr []int) Tower {
+func NewTower(arr []int) (Tower, int) {
 	sort.Sort(sort.Reverse(sort.IntSlice(arr)))
-	return Tower{arr, make([]int, 0, len(arr)), make([]int, 0, len(arr))}
+	return Tower{arr, make([]int, 0, len(arr)), make([]int, 0, len(arr))}, len(arr)
 }
 
-type ArrayType int
-
-const (
-	startArr ArrayType = 0
-	endArr   ArrayType = 1
-	tmpArr   ArrayType = 2
-)
-
-func (t *Tower) DefineArr(arrtype ArrayType) *[]int {
-	var arr *[]int
-	switch arrtype {
-	case startArr:
-		arr = &t.start
-	case endArr:
-		arr = &t.end
-	case tmpArr:
-		arr = &t.tmp
-	}
-	return arr
-}
-
-func (t *Tower) RearrElement(arrtype ArrayType, index int) {
-	arr := t.DefineArr(arrtype)
-
-	for i := 0; i < 3; i++ {
-		if i == int(arrtype) {
-			continue
-		}
-		switch {
-		case i == 0 && (len(t.start) == 0 || t.start[len(t.start)-1] > (*arr)[index]) && index%2 != 0:
-			t.start = append(t.start, (*arr)[index])
-			*arr = (*arr)[:len(*arr)-1]
-
-		case i == 1 && (len(t.end) == 0 || t.end[len(t.end)-1] > (*arr)[index]) && index%2 == 0:
-			t.end = append(t.end, (*arr)[index])
-			*arr = (*arr)[:len(*arr)-1]
-
-		case i == 2 && (len(t.tmp) == 0 || t.tmp[len(t.tmp)-1] > (*arr)[index]) && index%2 != 0:
-			t.tmp = append(t.tmp, (*arr)[index])
-			*arr = (*arr)[:len(*arr)-1]
-		}
-	}
-
-}
-
-func Hanoi(index int, t *Tower, arrtype ArrayType) {
-	arr := t.DefineArr(arrtype)
-
-	if index == len(*arr)-1 {
-		fmt.Println("end ", t.start[index], index)
-		t.RearrElement(arrtype, len(t.start)-1)
+func (t *Tower) Hanoi(src, dest, tmp *[]int, n int) {
+	if n == 0 {
+		transposit(src, dest)
 		return
 	}
-	fmt.Println("enter ", t.start[index])
-	Hanoi(index+1, t, arrtype)
-	fmt.Println("exit ", t.start[index])
+
+	t.Hanoi(src, tmp, dest, n-1)
+	if len(*src) != 0 {
+		transposit(src, dest)
+	}
+	t.Hanoi(tmp, dest, src, n-1)
+}
+
+func transposit(source, dest *[]int) {
+	*dest = append((*dest), (*source)[len(*source)-1])
+	*source = (*source)[:len(*source)-1]
 }
